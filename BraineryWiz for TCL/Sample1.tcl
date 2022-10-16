1d0acbf1c03d99ac55cc8e9b3f20999d1ded4cd9
Bijan Sayyafzadeh <b.sayyaf@yahoo.com>
2022-10-16 22:15:35 +0330

    Feed Initial Info

--- BraineryWiz for TCL/Sample1.tcl
+++ BraineryWiz for TCL/Sample1.tcl
@@ -1,3 +1,24 @@
+# /* ****************************************************************** **
+# **                    BraineryWiz TCL version                         **
+# **              by Bijan Sayyafzadeh & Silvia Mazzoni                 **
+# **                                                                    **
+# **                                                                    **
+# ** (C) Copyright 2022, Bijan Sayyafzadeh & Silvia Mazzoni             **
+# **       SilviaBrainery.com                                           **
+# ** All Rights Reserved.                                               **
+# **                                                                    **
+# ** Commercial use of this program without express permission of the   **
+# ** https://www.silviasbrainery.com/ (BraineryWiz@Gmail.com), is       **
+# ** strictly prohibited.  See file 'COPYRIGHT'  in main directory      **
+# ** for information on usage and redistribution,  and for a            **
+# ** DISCLAIMER OF ALL WARRANTIES.                                      **
+# **                                                                    **
+# ** Developed by:                                                      **
+# **   Bijan SayyafZadeh (B.Sayyaf@yahoo.com)                           **
+# **   SilviaMazzoni     (SilviaMazzoni@Yahoo.com)                      **
+# **                                                                    **
+# ** ****************************************************************** */
+
 source BraineryWiz.tcl
 
 wipe
