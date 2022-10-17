69d2abaf73f4d9650ba8b9fe1687941476dda26e
Bijan Sayyafzadeh <b.sayyaf@yahoo.com>
2022-10-17 11:11:29 +0330

    Update __init__.py

--- BraineryWiz/__init__.py
+++ BraineryWiz/__init__.py
@@ -23,6 +23,6 @@ from .PlotModel import PlotModel
 from .PlotAnimation import RecorderReset, Record, PlotAnime, PlotAnimeGif
 from .PlotDefo import PlotDefo
 from .PlotModeShape import PlotModeShape
-from .RealTime import RealTimeObj, ReatTimeUpdate
+from .RealTime import RealTimeObj, RealTimeUpdate
 from .FEMWiz import BaseData, DispData, EigenData
 
