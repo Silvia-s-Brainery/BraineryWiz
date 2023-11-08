245f9c40b9b20b152f16175f50144bfd98cd9d2c
Bijan Sayyafzadeh <82403633+BijanSeif@users.noreply.github.com>
2023-11-09 01:19:01 +0330

    update __init__
    Update to prevent the error messages for other system platforms except windows

--- BraineryWiz/__init__.py
+++ BraineryWiz/__init__.py
@@ -19,10 +19,45 @@
 # **                                                                    **
 # ** ****************************************************************** */
 
-from .PlotModel import PlotModel
-from .PlotAnimation import RecorderReset, Record, PlotAnime, PlotAnimeGif
-from .PlotDefo import PlotDefo
-from .PlotModeShape import PlotModeShape
-from .RealTime import RealTimeObj, RealTimeUpdate
-from .FEMWiz import BaseData, DispData, EigenData
+import sys
 
+# only work for 64 bit system
+if sys.maxsize < 2**31:
+    raise RuntimeError('64 bit system is required')
+
+# platform dependent
+if sys.platform.startswith('linux'):
+
+    try:
+        # from openseespylinux.opensees import *
+        raise RuntimeError(sys.platform+' is not supported yet')
+
+    except:
+        raise RuntimeError('Failed to import openseespy on Linux.')
+
+elif sys.platform.startswith('win'):
+
+    try:
+        from .PlotModel import PlotModel
+        from .PlotAnimation import RecorderReset, Record, PlotAnime, PlotAnimeGif
+        from .PlotDefo import PlotDefo
+        from .PlotModeShape import PlotModeShape
+        from .RealTime import RealTimeObj, RealTimeUpdate
+        from .FEMWiz import BaseData, DispData, EigenData
+
+    except:
+        raise RuntimeError('Failed to import openseespy on Windows. Attention: Anaconda environment is not supported yet. For more information visit: https://silvia-s-brainery.github.io/BraineryWizDocumentation/Python_Version/installation.html')
+
+elif sys.platform.startswith('darwin'):
+
+    try:
+        # from openseespymac.opensees import *
+        raise RuntimeError(sys.platform+' is not supported yet')
+
+    except:
+        raise RuntimeError('Failed to import openseespy on Mac.')
+
+
+else:
+
+    raise RuntimeError(sys.platform+' is not supported yet')
